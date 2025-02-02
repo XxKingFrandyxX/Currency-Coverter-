a497a537b2994d893486db90c94655eb85301e26
XxKingFrandyxX <151718434+XxKingFrandyxX@users.noreply.github.com>
2025-02-02 14:27:17 +1100

    Finished
    Done!!!

--- Form1.vb
+++ Form1.vb
@@ -3,17 +3,17 @@
         Dim value As Integer
         value = TXTmoney.Text
 
-        If RBone.Enabled = True Then
+        If RBone.Checked = True Then
             value = value * 0.78
         Else
             value = value * 1
         End If
-        If RBtwo.Enabled = True Then
+        If RBtwo.Checked = True Then
             value = value * 0.5
         Else
             value = value * 1
         End If
-        If RBthree.Enabled = True Then
+        If RBthree.Checked = True Then
             value = value * 0.6
         Else
             value = value * 1
@@ -21,5 +21,9 @@
 
         LBLoutput.Text = value
 
+    End Sub
+
+    Private Sub BTNquit_Click(sender As Object, e As EventArgs) Handles BTNquit.Click
+        Application.Exit()
     End Sub
 End Class
